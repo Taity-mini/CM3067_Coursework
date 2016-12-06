a4a8bcdc488b4df7428180609e290efcaf50a812
Taity-mini <1504693@rgu.ac.uk>
2016-12-06 18:45:45 +0000

    Project Started
    App Icon completed
    Launch Screen Completed
    Initial Split screen controller added

---
+++ CM3067_Coursework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CM3067_Coursework
+//
+//  Created by Andrew Tait on 30/10/2016.
+//  Copyright © 2016 Andrew Tait. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CM3067_CourseworkTests/CM3067_CourseworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  CM3067_CourseworkTests.swift
+//  CM3067_CourseworkTests
+//
+//  Created by Andrew Tait on 30/10/2016.
+//  Copyright © 2016 Andrew Tait. All rights reserved.
+//
+
+import XCTest
+@testable import CM3067_Coursework
+
+class CM3067_CourseworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
